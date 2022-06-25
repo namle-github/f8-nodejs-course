fc242a0f5d096df4f7b20664f32ef69e08de6ec2
leanhnam <leanhnam94@gmail.com>
2022-06-25 09:48:13 +0700

    [F8] Static files and SCSS

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,6 +5,9 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// Static file
+app.use(express.static(path.join(__dirname, 'public')))
+
 // HTTP logger
 app.use(morgan('combined'))
 
@@ -12,8 +15,6 @@ app.use(morgan('combined'))
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'))
-
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.render('home');
